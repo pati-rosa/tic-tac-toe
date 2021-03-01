58bd5e8a2e617c9d13163ba81f029e9658da0e3e
patirosa95 <patirosa995@gmail.com>
2021-03-01 16:01:30 -0300

    added state to Board

--- src/index.js
+++ src/index.js
@@ -3,18 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+            className="square" 
+            onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        squares:Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return <Square 
+        value={this.state.squares[i]} 
+        onClick={()=>this.handleClick(i)}
+        />;
     }
   
     render() {
@@ -54,6 +74,7 @@ class Square extends React.Component {
             <div>{/* status */}</div>
             <ol>{/* TODO */}</ol>
           </div>
+          
         </div>
       );
     }
