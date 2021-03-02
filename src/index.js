7fa356879bd6b1bea5e08d91d7753d495fbeb3c8
patirosa95 <patirosa995@gmail.com>
2021-03-02 17:25:44 -0300

    Passing the "Board" class component to function

--- src/index.js
+++ src/index.js
@@ -13,36 +13,55 @@ function Square(props) {
       );
   }
   
-  class Board extends React.Component {
+  function Board (props){
 
-    renderSquare(i) {
-      return <Square 
-        value={this.props.squares[i]} 
-        onClick={()=>this.props.onClick(i)}
-        />;
-    }
-  
-    render() { 
       return (
         <div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            <Square 
+              value={props.squares[1]} 
+              onClick={()=>props.onClick(1)}
+            />
+            <Square 
+              value={props.squares[1]} 
+              onClick={()=>props.onClick(2)}
+            />
+            <Square 
+              value={props.squares[2]} 
+              onClick={()=>props.onClick(2)}
+            />           
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            <Square 
+                value={props.squares[3]} 
+                onClick={()=>props.onClick(3)}
+              />
+            <Square 
+              value={props.squares[4]} 
+              onClick={()=>props.onClick(4)}
+            />
+            <Square 
+              value={props.squares[5]} 
+              onClick={()=>props.onClick(5)}
+            />
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            <Square 
+              value={props.squares[6]} 
+              onClick={()=>props.onClick(6)}
+            />
+            <Square 
+              value={props.squares[7]} 
+              onClick={()=>props.onClick(7)}
+            />
+            <Square 
+              value={props.squares[8]} 
+              onClick={()=>props.onClick(8)}
+            />
           </div>
         </div>
       );
-    }
+
   }
   
   class Game extends React.Component {
